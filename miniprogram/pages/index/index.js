dc76e3d07729441bbb2d37bcf4f5a3311157c288
LinuxZhou93 <406371738@qq.com>
2026-05-07 11:53:50 +0800

    feat: enable true hot-update with CSS cache busting

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,7 +1,10 @@
 // pages/index/index.js
 Page({
   data: {
-    // 直接访问 index.html，避开根路径的中间件拦截
-    url: 'https://www.zhouxiaomai.com/index.html?ui_mode=miniprogram'
+    // 使用更高版本的部署标签，强制微信重新拉取
+    url: 'https://www.zhouxiaomai.com/index.html?ui_mode=miniprogram&deploy_v=20260507_v5.6_FINAL'
+  },
+  onLoad(options) {
+    console.log("🚀 [FutureClass] Force Reloading HUD: V5.6_FINAL");
   }
 })
--- miniprogram/pages/profile/index.js
+++ miniprogram/pages/profile/index.js
@@ -1,1 +1,6 @@
-Page({ data: { url: 'https://www.zhouxiaomai.com/resources/profile.html?ui_mode=miniprogram' } })
+// pages/profile/index.js
+Page({
+  data: {
+    url: 'https://www.zhouxiaomai.com/resources/profile.html?ui_mode=miniprogram&deploy_v=20260507_v5.6_FINAL'
+  }
+})
